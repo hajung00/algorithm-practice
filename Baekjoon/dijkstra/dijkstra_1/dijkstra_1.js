25f56091712f7f44d67b9072445afa0660bac73a
hajung00 <gkwjd719@naver.com>
2024-02-21 17:37:20 +0900

    2024-02-21: dijkstra 1

---
+++ Baekjoon/dijkstra/dijkstra_1/dijkstra_1.js
@@ -0,0 +1,243 @@
+// 1753. 최단경로
+
+// 🏷️ 문제
+// 방향그래프가 주어지면 주어진 시작점에서 다른 모든 정점으로의 최단 경로를 구하는 프로그램을 작성하시오. 단, 모든 간선의 가중치는 10 이하의 자연수이다.
+
+// 🏷️ 입력
+// 첫째 줄에 정점의 개수 V와 간선의 개수 E가 주어진다. (1 ≤ V ≤ 20,000, 1 ≤ E ≤ 300,000) 모든 정점에는 1부터 V까지 번호가 매겨져 있다고 가정한다. 둘째 줄에는 시작 정점의 번호 K(1 ≤ K ≤ V)가 주어진다. 셋째 줄부터 E개의 줄에 걸쳐 각 간선을 나타내는 세 개의 정수 (u, v, w)가 순서대로 주어진다. 이는 u에서 v로 가는 가중치 w인 간선이 존재한다는 뜻이다.
+// u와 v는 서로 다르며 w는 10 이하의 자연수이다. 서로 다른 두 정점 사이에 여러 개의 간선이 존재할 수도 있음에 유의한다.
+
+// 🏷️ 출력
+// 첫째 줄부터 V개의 줄에 걸쳐, i번째 줄에 i번 정점으로의 최단 경로의 경로값을 출력한다. 시작점 자신은 0으로 출력하고, 경로가 존재하지 않는 경우에는 INF를 출력하면 된다.
+
+// 🏷️ 예제 입출력
+
+// 입력
+// 5 6
+// 1
+// 5 1 1
+// 1 2 2
+// 1 3 3
+// 2 3 4
+// 2 4 5
+// 3 4 6
+
+// 출력
+// 0
+// 2
+// 3
+// 7
+// INF
+
+/**
+ * Expose `PriorityQueue`.
+ */
+module.exports = PriorityQueue;
+
+/**
+ * Initializes a new empty `PriorityQueue` with the given `comparator(a, b)`
+ * function, uses `.DEFAULT_COMPARATOR()` when no function is provided.
+ *
+ * The comparator function must return a positive number when `a > b`, 0 when
+ * `a == b` and a negative number when `a < b`.
+ *
+ * @param {Function}
+ * @return {PriorityQueue}
+ * @api public
+ */
+function PriorityQueue(comparator) {
+  this._comparator = comparator || PriorityQueue.DEFAULT_COMPARATOR;
+  this._elements = [];
+}
+
+/**
+ * Compares `a` and `b`, when `a > b` it returns a positive number, when
+ * it returns 0 and when `a < b` it returns a negative number.
+ *
+ * @param {String|Number} a
+ * @param {String|Number} b
+ * @return {Number}
+ * @api public
+ */
+PriorityQueue.DEFAULT_COMPARATOR = function (a, b) {
+  if (typeof a === "number" && typeof b === "number") {
+    return a - b;
+  } else {
+    a = a.toString();
+    b = b.toString();
+
+    if (a == b) return 0;
+
+    return a > b ? 1 : -1;
+  }
+};
+
+/**
+ * Returns whether the priority queue is empty or not.
+ *
+ * @return {Boolean}
+ * @api public
+ */
+PriorityQueue.prototype.isEmpty = function () {
+  return this.size() === 0;
+};
+
+/**
+ * Peeks at the top element of the priority queue.
+ *
+ * @return {Object}
+ * @throws {Error} when the queue is empty.
+ * @api public
+ */
+PriorityQueue.prototype.peek = function () {
+  if (this.isEmpty()) throw new Error("PriorityQueue is empty");
+
+  return this._elements[0];
+};
+
+/**
+ * Dequeues the top element of the priority queue.
+ *
+ * @return {Object}
+ * @throws {Error} when the queue is empty.
+ * @api public
+ */
+PriorityQueue.prototype.deq = function () {
+  var first = this.peek();
+  var last = this._elements.pop();
+  var size = this.size();
+
+  if (size === 0) return first;
+
+  this._elements[0] = last;
+  var current = 0;
+
+  while (current < size) {
+    var largest = current;
+    var left = 2 * current + 1;
+    var right = 2 * current + 2;
+
+    if (left < size && this._compare(left, largest) >= 0) {
+      largest = left;
+    }
+
+    if (right < size && this._compare(right, largest) >= 0) {
+      largest = right;
+    }
+
+    if (largest === current) break;
+
+    this._swap(largest, current);
+    current = largest;
+  }
+
+  return first;
+};
+
+/**
+ * Enqueues the `element` at the priority queue and returns its new size.
+ *
+ * @param {Object} element
+ * @return {Number}
+ * @api public
+ */
+PriorityQueue.prototype.enq = function (element) {
+  var size = this._elements.push(element);
+  var current = size - 1;
+
+  while (current > 0) {
+    var parent = Math.floor((current - 1) / 2);
+
+    if (this._compare(current, parent) <= 0) break;
+
+    this._swap(parent, current);
+    current = parent;
+  }
+
+  return size;
+};
+
+/**
+ * Returns the size of the priority queue.
+ *
+ * @return {Number}
+ * @api public
+ */
+PriorityQueue.prototype.size = function () {
+  return this._elements.length;
+};
+
+/**
+ *  Iterates over queue elements
+ *
+ *  @param {Function} fn
+ */
+PriorityQueue.prototype.forEach = function (fn) {
+  return this._elements.forEach(fn);
+};
+
+/**
+ * Compares the values at position `a` and `b` in the priority queue using its
+ * comparator function.
+ *
+ * @param {Number} a
+ * @param {Number} b
+ * @return {Number}
+ * @api private
+ */
+PriorityQueue.prototype._compare = function (a, b) {
+  return this._comparator(this._elements[a], this._elements[b]);
+};
+
+/**
+ * Swaps the values at position `a` and `b` in the priority queue.
+ *
+ * @param {Number} a
+ * @param {Number} b
+ * @api private
+ */
+PriorityQueue.prototype._swap = function (a, b) {
+  var aux = this._elements[a];
+  this._elements[a] = this._elements[b];
+  this._elements[b] = aux;
+};
+
+let fs = require("fs");
+let input = fs.readFileSync("dijkstra_1.txt").toString().split("\n");
+
+let INF = 1e9;
+let [v, e] = input[0].split(" ").map(Number);
+let start = Number(input[1]);
+let graph = [Array(v + 1).fill(0)];
+let distance = Array(v + 1).fill(INF);
+
+for (let i = 1; i <= v; i++) graph[i] = [];
+for (let i = 2; i <= e + 1; i++) {
+  let [x, y, d] = input[i].split(" ").map(Number);
+  graph[x].push([y, d]);
+}
+
+function dijkstra() {
+  let pq = new PriorityQueue((a, b) => b[0] - a[0]);
+  pq.enq([0, start]);
+  distance[start] = 0;
+
+  while (pq.size() !== 0) {
+    let [dis, node] = pq.deq();
+    if (distance[node] < dis) continue;
+    for (let i of graph[node]) {
+      let cost = dis + i[1];
+      if (cost < distance[i[0]]) {
+        distance[i[0]] = cost;
+        pq.enq([cost, i[0]]);
+      }
+    }
+  }
+}
+
+dijkstra();
+
+for (let i = 1; i <= v; i++) {
+  if (distance[i] == INF) console.log("INF");
+  else console.log(distance[i]);
+}
